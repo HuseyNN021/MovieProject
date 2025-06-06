0d316ec712120aec6859c5ea5307f647c3ff2641
HuseyNN021 <hhsnov880@gamil.com>
2025-06-06 16:10:15 -0700

    add  simple password validation to Sign.js

--- sign.js
+++ sign.js
@@ -5,14 +5,35 @@ let passwordInput = document.getElementById('password')
 
 
 
-submitBtn.addEventListener("click",(event)=>{
-
-
-    if(passwordInput.value.length<5){
-        alert("Your password too short")
-    }
-
+submitBtn.addEventListener("click", (event) => {
     event.preventDefault();
 
-})
+    let valueOfInput = passwordInput.value;
+    const minLength = /.{8,}/;
+    const hasUpperCase = /[A-Z]/;
+    const hasLowerCase = /[a-z]/;
+    const hasNumber = /[0-9]/;
+    const hasSpecialChar = /[!@#$%^&*]/;
 
+    if (!minLength.test(valueOfInput)) {
+        alert("Parol ən azı 8 simvoldan ibarət olmalıdır.");
+    }
+
+    if (!hasUpperCase.test(valueOfInput)) {
+        alert("Parol ən azı bir böyük hərf içerməlidir.");
+    }
+
+    if (!hasLowerCase.test(valueOfInput)) {
+        alert("Parol ən azı bir kiçik hərf içerməlidir.");
+    }
+
+    if (!hasNumber.test(valueOfInput)) {
+        alert("Parol ən azı bir rəqəm içerməlidir.");
+    }
+
+    if (!hasSpecialChar.test(valueOfInput)) {
+        alert("Parol ən azı bir xüsusi simvol (!@#$%^&*) içerməlidir.");
+    }
+    alert("Parol doğrudur! Form göndərilə bilər.");
+
+})
